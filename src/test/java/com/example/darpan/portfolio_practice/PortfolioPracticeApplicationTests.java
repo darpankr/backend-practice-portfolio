59c79193f8b426693d2951d05aa294cb5718729a
UserDarpan <darpankumar.sing@clarivate.com>
2026-06-14 22:00:46 +0530

    Fix cicd jdbc test

--- src/test/java/com/example/darpan/portfolio_practice/PortfolioPracticeApplicationTests.java
+++ src/test/java/com/example/darpan/portfolio_practice/PortfolioPracticeApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("prod")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PortfolioPracticeApplicationTests {
 
 	@Test
