6c606721da8a4766036f7f1b394240688df6a7e6
UserDarpan <darpankumar.sing@clarivate.com>
2026-06-14 21:33:45 +0530

    Added profiles and connect to aws mysql

--- src/test/java/com/example/darpan/portfolio_practice/PortfolioPracticeApplicationTests.java
+++ src/test/java/com/example/darpan/portfolio_practice/PortfolioPracticeApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.darpan.portfolio_practice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class PortfolioPracticeApplicationTests {
 
 	@Test
