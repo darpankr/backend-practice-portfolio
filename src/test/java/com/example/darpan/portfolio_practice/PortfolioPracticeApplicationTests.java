4b8c2fcfdd9c6fe8c196ac5183833f13c01c80ac
UserDarpan <darpankumar.sing@clarivate.com>
2026-06-14 21:47:51 +0530

    Fix the congif and profile and db connection

--- src/test/java/com/example/darpan/portfolio_practice/PortfolioPracticeApplicationTests.java
+++ src/test/java/com/example/darpan/portfolio_practice/PortfolioPracticeApplicationTests.java
@@ -1,11 +1,13 @@
 package com.example.darpan.portfolio_practice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("dev")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class PortfolioPracticeApplicationTests {
 
 	@Test
